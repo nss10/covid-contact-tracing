a22140aef71baaea4345f2bc971f264316de7fbc
Shanmukha <nss10@outlook.com>
2020-07-25 21:47:06 -0500

    Added few basic algorithms

--- server/algorithms.py
+++ server/algorithms.py
@@ -1,35 +1,69 @@
 from datetime import datetime as dt
-def room_entry(event):
-    event={"code":"1432-0","uid":"2309","ts":dt(2020,4,23,16,23,34,123)}
-    room_id,status=event['code'].split('-')
-    user_id,timeStamp=event['uid'],event['ts']
-    print(room_id,user_id,timeStamp,status)
-    isJanitor(user_id)
-    #Add event info to db
-    
-    #Update room capacity
-    update_room_capacity(event)
-    #IF event type is entry
-        #Return room name and last sanitized time
-    #Else
-        #Return time spent in the room
-    '''
-    if(status ==0):
-        return fetch_room_info(event.room_id)
-    else:
-        return fetch_time_spent(event.user_id, event.room_id)
-    '''
-def add_sanitized_event(sanitize_event):
-    pass
+from db_interface import *
 
-def update_room_capacity(event):
-    pass
+
+def room_entry(event):
+    room_id, status = event['code'].split('-')
+    user_id = event['uid']
+    # print(room_id,user_id,timeStamp,status)
+    room_info = fetch_room_info(room_id)
+    if isJanitor(user_id):
+        room_info['clean_prompt'] = 1
+    else:
+        handle_missing_events(room_id, user_id, status)
+
+        add_event(event)
+        update_room_capacity(room_id, user_id, status)
+    return room_info
+
+
+def room_exit(event):
+    room_id, status = event['code'].split('-')
+    user_id = event['uid']
+    if isJanitor(user_id):
+        room_clean_exit(event)
+    else:
+        handle_missing_events(user_id, room_id, status)
+        add_event(event)
+        update_room_capacity(room_id, user_id, status)
+        return {"code": 200, "status": "Success"}
+
+
+def update_room_capacity(room_id, user_id, status):
+    current_capacity = fetch_room_info(room_id)['current_capacity']
+    if status == 0:
+        set_room_capacity(room_id, current_capacity-1)
+    else:
+        set_room_capacity(room_id, current_capacity+1)
 
 
 def isJanitor(uid):
     pass
 
 
+def handle_missing_events(room_id, user_id, status):
+    old_rec = fetch_last_user_record(room_id, user_id)
+    #WIP
+    pass
+
+
+def room_clean(event):
+    room_id, status = event['code'].split('-')
+    timeStamp = event['ts']
+    add_sanitized_event(event)
+    if status == 0:
+        set_last_sanitized_ts(room_id, timeStamp)
+    else:
+        set_last_sanitized_ts(room_id, ts=None)
+
+
+def room_clean_exit(event):
+    room_id = event['code'].split('-')[0]
+    add_sanitized_event(event)
+    set_last_sanitized_ts(room_id, ts=None)
+
 
 if __name__ == "__main__":
-    room_entry(None)
+    event = {"code": "1432-0", "uid": "2309",
+             "ts": dt(2020, 4, 23, 16, 23, 34, 123)}
+    room_entry(event)
--- server/db_interface.py
+++ server/db_interface.py
@@ -1,12 +1,26 @@
+from datetime import datetime as dt
 def fetch_room_info(room_id):
     #return room{name, last_sanitized_time}
-    pass
+    return {"name":"pilion hall", "last_sanitized_time":dt(2020,4,23,19,13,34,123), "current_capacity":14, "max_capacity":20}
 
-def fetch_time_spent(user_id, room_id):
+def fetch_last_user_record(user_id, room_id):
     #Fetch the last event with user_id and room_id with status as 'Entry'
-    pass
+    return {"rid":room_id,"uid":user_id,"status":0,"timeStamp":dt(2020,4,23,19,13,34,123)}
 
 def add_event(event):
     #Insert this record in events collection
-    
     pass
+
+
+def add_sanitized_event(event):
+    #Insert this record in sanitized events collection
+    pass
+
+def set_room_capacity(room_id, capacity):
+    pass
+
+def set_last_sanitized_ts(room_id,ts):
+    if ts==None:
+        pass #insert ts = "Cleaning in progress"
+    else:   
+        pass #insert ts = given timestamp for that room
