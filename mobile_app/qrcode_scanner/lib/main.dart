bd04061972720014e066e662c426de6b17e8e4ac
Sai Shanmukha Narumanchi <nss10@outlook.com>
2020-10-23 15:35:11 -0500

    Modified QR Scan and removed NFC code

--- mobile_app/qrcode_scanner/lib/main.dart
+++ mobile_app/qrcode_scanner/lib/main.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
-
-import 'package:flutter_nfc_reader/flutter_nfc_reader.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/intl_standalone.dart';
+// import 'package:flutter_nfc_reader/flutter_nfc_reader.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,24 +38,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _result = "Text";
-  NfcData _nfcData;
+  var _result = "Please Scan the QR code";
+
+  static final dateFormat = new DateFormat('MMM d, yyyy hh:mm aaa');
+  Map<String, dynamic> _response;
+  Map<String, dynamic> responseTitles = {
+    "name": "Room Name",
+    "current_strength": "Current Strength",
+    "max_capacity": "Max Capacity",
+    "last_sanitized_time": "Last Sanitized Time",
+  };
+  // NfcData _nfcData;
   Future<http.Response> _sendRequest() async {
+    print(new DateTime.now().millisecondsSinceEpoch.toString());
+
+    var _result_split = new List(2);
+    _result_split = _result.split("-");
+
     http.Response response = await http.post(
-      'http://10.100.192.74:5000/addEvent',
+      'http://<your-ip-address>:5000/addEvent',
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        'room_id': "J28929",
+        'room_id': _result_split[0],
         'user_id': '70983',
-        'status': '1',
+        'status': _result_split[1],
         'timestamp': new DateTime.now().millisecondsSinceEpoch.toString()
       }),
     );
     setState(() {
-      _result = response.body;
+      _response = jsonDecode(utf8.decode(response.bodyBytes));
+      if (_response['last_sanitized_time'] != null) {
+        _response['last_sanitized_time'] =
+            dateFormat.format(DateTime.parse(_response['last_sanitized_time']));
+      }
     });
+    print("State updated!");
+    print(_response);
   }
 
   Future _scanQR() async {
@@ -72,48 +94,31 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Future<void> startNFC() async {
-    NfcData response;
-
-    setState(() {
-      _nfcData = NfcData();
-      _nfcData.status = NFCStatus.reading;
-    });
-
-    print('NFC: Scan started');
-
-    try {
-      print('NFC: Scan readed NFC tag');
-      response = await FlutterNfcReader.read();
-    } on PlatformException {
-      print('NFC: Scan stopped exception');
-    }
-    setState(() {
-      _nfcData = response;
-    });
+  Padding getTableElement(String value) {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Text(
+        value,
+        style: TextStyle(color: Colors.blue, fontSize: 20),
+        textAlign: TextAlign.left,
+      ),
+    );
   }
 
-  Future<void> stopNFC() async {
-    NfcData response;
-
-    try {
-      print('NFC: Stop scan by user');
-      response = await FlutterNfcReader.stop();
-    } on PlatformException {
-      print('NFC: Stop scan exception');
-      response = NfcData(
-        id: '',
-        content: '',
-        error: 'NFC scan stop exception',
-        statusMapper: '',
-      );
-      response.status = NFCStatus.error;
-      ;
+  List<Widget> populateCard() {
+    List<Widget> entryCard = new List<Widget>();
+    if (_response == null) {
+      entryCard.add(Image.network(
+        'https://www.clipartkey.com/mpngs/m/229-2295584_qr-code-scan-hand-scan-qr-code-png.png',
+        fit: BoxFit.fill,
+      ));
+    } else if (_response['code'] == 200) {
+      entryCard.add(getTableElement("Thank you for scanning!"));
+    } else {
+      responseTitles.forEach(
+          (k, v) => entryCard.add(getTableElement("$v : ${_response[k]}")));
     }
-
-    setState(() {
-      _nfcData = response;
-    });
+    return entryCard;
   }
 
   @override
@@ -126,21 +131,32 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Response Text',
+            Card(
+              elevation: 10,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: populateCard()),
             ),
-            Text(
-              '$_nfcData',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            SizedBox(height: 50),
+            RaisedButton.icon(
+                color: Colors.blue,
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(12.0),
+                splashColor: Colors.blueAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                onPressed: _scanQR,
+                label: Text(
+                  "Scan QR",
+                  style: TextStyle(fontFamily: 'Times New Roman', fontSize: 23),
+                ),
+                icon: Icon(Icons.camera_alt))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.camera_alt),
-          label: Text("Scan"),
-          onPressed: startNFC),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- server/API.py
+++ server/API.py
@@ -1,5 +1,6 @@
 from algorithms import add_event_algo
 from contact_tracing import Event
+# from datetime import date, datetime
 import datetime,json
 from flask import Flask, request, url_for, redirect
 app = Flask(__name__)
@@ -19,4 +20,12 @@ def add_event():
     event = getEventObj(request.get_json())
     event.timestamp=datetime.datetime.now()
     print(event.status)
-    return str(add_event_algo(event))
+    return json.dumps(add_event_algo(event), default=json_serial)
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
